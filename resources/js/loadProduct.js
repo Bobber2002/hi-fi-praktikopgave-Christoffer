ec1b89697fd6af68398c24eb537cbbbd5e530b4a
ChristofferRTS <4906240@rts-365.dk>
2021-10-05 15:07:44 +0200

    add img to dynamic item

--- resources/js/loadProduct.js
+++ resources/js/loadProduct.js
@@ -20,9 +20,44 @@ fetch('resources/json/productData.json')
                 item.appendChild(itemFlex);
                 itemFlex.className = "item__flexContainer";
 
+                var itemImg = document.createElement("img");
+                itemFlex.appendChild(itemImg);
+                itemImg.className="item__img";
+                itemImg.src = data[i].img;
+
                 var itemName = document.createElement("p");
                 itemFlex.appendChild(itemName);
                 itemName.className="item__name";
                 itemName.innerHTML = data[i].name;
+
+                var priceDiv = document.createElement("div");
+                itemFlex.appendChild(priceDiv);
+                priceDiv.className = "item__price";
+
+                var beforeDiscountPriceP = document.createElement("p");
+                priceDiv.appendChild(beforeDiscountPriceP);
+                beforeDiscountPriceP.className = "item__beforeDiscountPrice";
+                beforeDiscountPriceP.innerHTML = data[i].beforeDiscountPrice;
+
+                var currentPriceP = document.createElement("p");
+                priceDiv.appendChild(currentPriceP);
+                currentPriceP.className = "item__currentPrice";
+                currentPriceP.innerHTML = data[i].currentPrice;
+
+                var addToCartA = document.createElement("a");
+                itemFlex.appendChild(addToCartA);
+                addToCartA.className="item__addToCart";
+
+                var addToCartDiv = document.createElement("div");
+                addToCartA.appendChild(addToCartDiv);
+                addToCartDiv.className="addToCart__div"
+
+                var addToCartP = document.createElement("p");
+                addToCartDiv.appendChild(addToCartP);
+                addToCartP.className = "addToCart__p";
+                addToCartP.innerHTML = "ADD TO CART";
+
+
+                
             }
         }
